04cb25692d09b43a19ffa0179d9431f48b7f3f02
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-04-25 15:27:08 -0300

    format: formatting code

--- apps/escola/views/course_views.py
+++ apps/escola/views/course_views.py
@@ -1,7 +1,8 @@
+from rest_framework import viewsets
+
 from apps.escola.models.course_model import Course
 from apps.escola.serializers.course_serializer import CourseSerializer
 
-from rest_framework import viewsets
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
