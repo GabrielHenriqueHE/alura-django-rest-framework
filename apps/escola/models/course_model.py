0e5ac7ff8ee9b13e6782a1217073b447df984fcc
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-04-29 14:58:59 -0300

    feat: implementing Model validations

--- apps/escola/models/course_model.py
+++ apps/escola/models/course_model.py
@@ -1,10 +1,17 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
 class Course(models.Model):
     LEVEL = (("B", "Basic"), ("I", "Intermediary"), ("A", "Advanced"))
 
-    code = models.CharField(blank=False, max_length=10, null=False)
+    code = models.CharField(
+        blank=False,
+        max_length=10,
+        null=False,
+        unique=True,
+        validators=[MinLengthValidator(3)],
+    )
     description = models.CharField(blank=False)
     level = models.CharField(blank=False, choices=LEVEL, default="B", null=False)
 
--- apps/escola/models/student_model.py
+++ apps/escola/models/student_model.py
@@ -4,7 +4,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(blank=False, max_length=100, null=False)
     email = models.EmailField(blank=False, max_length=100, null=False)
-    cpf = models.CharField(blank=False, max_length=11, null=False)
+    cpf = models.CharField(blank=False, max_length=11, null=False, unique=True)
     birth_date = models.DateField(null=False)
     phone_number = models.CharField(blank=False, max_length=14, null=False)
 
